13085938e38b5bfbd9979b1dc82d8a1c63d8b4b4
edonisshabani <edonisshabani@hotmail.com>
2024-12-08 03:40:39 +0100

    9th commit ES

--- script/main.js
+++ script/main.js
@@ -1,3 +1,12 @@
 document.querySelector('.mob-menu').onclick = () => {
     document.querySelector('.navlist').classList.toggle('show');
 };
+
+window.onscroll = function() {
+    let header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        header.style.backgroundColor = 'rgba(255, 255, 255, 0.98)';
+    } else {
+        header.style.backgroundColor = 'transparent';
+    }
+};
